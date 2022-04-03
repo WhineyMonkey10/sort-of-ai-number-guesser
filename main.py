179159abb06737b2815e03815a1a68a6841036b6
WhineyMonkey10 <gbialystokjoly@gmail.com>
2022-04-03 16:46:36 +0200

    Final

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import random, math
+import random
 import time
 
 # Main
-def roundup(x):
-    return int(math.ceil(x / 100.0)) * 100
 def time_convert(sec):
     mins = sec // 60
     sec = sec % 60
@@ -12,7 +10,7 @@ def time_convert(sec):
     return "Time Lapsed = {0}:{1}:{2}".format(int(hours), int(mins),sec)
 
 numberinput = int(input("How big do you want the range of the number to be: "))
-number2guess = random.randint(1, roundup(numberinput))
+number2guess = random.randint(1, numberinput)
 show_guesses = input("Do you want it to log all the guesses (spam likely, it will also greatly reduce performance) y/n: ")
 guesses = 0
 while True:
