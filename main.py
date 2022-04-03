f8ce1729ebf9a8bc94060d66dd0f91f143dd79ff
WhineyMonkey10 <gbialystokjoly@gmail.com>
2022-04-03 16:37:07 +0200

    Added a user input to see the guesses.

--- main.py
+++ main.py
@@ -10,16 +10,19 @@ def time_convert(sec):
     mins = mins % 60
     return "Time Lapsed = {0}:{1}:{2}".format(int(hours), int(mins),sec)
 
-number2guess = random.randint(1, 10000)
-
+number2guess = random.randint(1, 1000000)
+show_guesses = input("Do you want it to log all the guesses (spam likely) y/n: ")
 
 guesses = 0
 while True:
     start_time = time.time()
-    computer_guess = random.randint(1, 10000)
+    computer_guess = random.randint(1, 1000000)
+    if show_guesses == 'y':
+        print(computer_guess)
     if computer_guess == number2guess:
         end_time = time.time()
         time_lapsed = end_time - start_time
-        print(f"The computer has guessed the number! \nIt took {guesses + 1} guesses and it took {time_convert(time_lapsed)} to find.")
+        print(f"The computer has guessed the number! \nIt took {guesses + 1} guesses and it took {time_convert(time_lapsed)} to find. The number was {number2guess}.")
+        input("Press enter to exit")
         break
     guesses += 1
